69050169bdd6fbea0f3a54ef22b3143d369b1058
Isidro Quevedo <isidroquevedo@hotmail.com>
2019-05-22 21:04:49 -0700

    Get the best match friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friendsData = [
     {
-      name: "Ahmed",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+      name: "John McCalister",
+      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
       scores: [
-        "5",
-        "1",
         "4",
         "4",
-        "5",
-        "1",
-        "2",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
         "5",
         "4",
-        "1"
+        "4"
       ]
     },
     {
-      name: "Jacob Deming",
+      name: "Antony Bruce",
       photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
       scores: [
         "4",
@@ -32,23 +32,23 @@ var friendsData = [
       ]
     },
     {
-      name: "Jeremiah Scanlon",
+      name: "Jesus Alvarez",
       photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
       scores: [
         "5",
-        "2",
-        "2",
-        "2",
-        "4",
-        "1",
-        "3",
-        "2",
         "5",
-        "5"
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "4"
       ]
     },
     {
-      name: "Louis T. Delia",
+      name: "Robert Smith",
       photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
       scores: [
         "3",
@@ -64,8 +64,8 @@ var friendsData = [
       ]
     },
     {
-      name: "Lou Ritter",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+      name: "Lawrence Sullivan",
+      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
       scores: [
         "4",
         "3",
@@ -80,8 +80,8 @@ var friendsData = [
       ]
     },
     {
-      name: "Jordan Biason",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+      name: "Johan Anderson",
+      photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
       scores: [
         "4",
         "4",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,44 @@
 var friends = require("../data/friends");
 
-console.log("friends", friends)
+/* console.log("friends", friends); */
 
 module.exports = function (app) {
 
     app.get("/api/friends", function (req, res) {
         console.log("get ruta api/friends")
         res.json(friends)
-      ///
+        ///
     });
-  
+
     app.post('/api/friends', function (req, res) {
-        console.log("post ruta api/friends");
+        /*      console.log("post ruta api/friends"); */
         var b = req.body;
-        console.log(b);
+        /*     console.log(b); */
 
         // buscar en arreglo friends el best matching   [3,2,1]   [3,5,1] = 2+3+2=7 / 0+3+0=3
 
-     ///
+        var total = 100;
+        var totalActual = 60;
+        var friend = 0;
+
+        for (let i = 0; i < friends.length; i++) {
+
+
+            total = 0;
+
+            for (let y = 0; y < 10; y++) {
+
+                total += Math.abs(parseInt(b.scores[y]) - parseInt(friends[i].scores[y]));
+
+                if (total < totalActual) {
+                    friend = i;
+                    totalActual = total;
+                };
+            }
+
+        }
+        console.log(friends[friend]);
+        return res.json(friends[friend]);
+        ///
     });
-  }
+}
